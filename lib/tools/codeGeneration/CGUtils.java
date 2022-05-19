5c1bbcda773d4b4ccb338c7a843d9ca2264f50e1
Diego Caballé <diegocaballe@hotmail.com>
2022-05-19 11:12:07 +0200

    Package resolution

---
+++ lib/tools/codeGeneration/CGUtils.java
@@ -0,0 +1,23 @@
+//*****************************************************************
+// File:   CGUtils.java
+// Author: Procesadores de Lenguajes-University of Zaragoza
+// Date:   abril 2022
+// Coms:   Librería con el método para crear etiquetas frescas y 
+// 			una variable estática para llevar la traza de las 
+// 			direcciones de memoria 
+//*****************************************************************
+
+package lib.tools.codeGeneration;
+
+public class CGUtils {
+	
+	private static int l=0; 
+	
+	public static String newLabel () {
+		return "L"+(l++); 
+	}
+	
+	// campo requeridos para la generación de código
+	public static int memorySpaces[] = new int[100];
+	
+}
